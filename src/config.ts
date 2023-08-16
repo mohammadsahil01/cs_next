15004f3686917e82bab6463431e9e84dbe24c122
mohammadsahil01 <mohammadsahil01@gmail.com>
2023-08-16 16:21:07 +0530

    added files

---
+++ src/config.ts
@@ -0,0 +1,2 @@
+export const BASE_URL = "http://week10.100xdevs.com";
+export const NEXT_URL = "http://localhost:3001";
--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -9,5 +9,6 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  console.log("hi there");
   res.status(200).json({ name: 'John Doe' })
 }
